f566e9afcbd78f77e8197f7169f31d91211b4972
msc-ljperez <48493617+msc-ljperez@users.noreply.github.com>
2020-07-15 19:20:17 -0500

    Update protection.js

--- protection.js
+++ protection.js
@@ -6,5 +6,3 @@ function listenToYoda() {
                               alert("Para aprender, teclear el código necesario es.\nCaer en las trampas del lado oscuro tu debes no, joven Padawan.\n\n    - Yoda");
                           }
                     }
-
-return listenToYoda;
